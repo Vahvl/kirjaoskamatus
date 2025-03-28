7603faaf4c520bbc68f33f36553f31e4dbad76cc
Vahvl <74665243+Vahvl@users.noreply.github.com>
2025-03-28 10:25:53 +0000

    Co-authored-by: LennartLuud <LennartLuud@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 # flask vist
-from flask import Flask
-Flask.url_for('static', filename='style.css')
-Flask.url_for('static', filename='main.html')
+from flask import *
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+url_for("static", filename="style.css")
+url_for("static",  filename="main.html")
