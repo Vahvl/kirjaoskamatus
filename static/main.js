70fe06e3b776c2de41d74187886c2e521dc4886b
Vahvl <74665243+Vahvl@users.noreply.github.com>
2025-04-11 10:31:56 +0000

    jesse me KÜPSETASIOME
    Co-authored-by: LennartLuud <LennartLuud@users.noreply.github.com>

--- static/main.js
+++ static/main.js
@@ -6,9 +6,11 @@ document.addEventListener('keydown', (event) => {
     if (keyPressed == "Backspace") {
         koik = koik.slice(0, -1)
         document.getElementById("koikTahed").innerHTML=koik
+        document.getElementById("bigAssTaht").innerHTML=koik.slice(-9,-1)
     }
-    else if (keyPressed != "Shift" || keyPressed != "Control"){
+    else if (keyPressed.length==1) {
         koik+=keyPressed
         document.getElementById("koikTahed").innerHTML=koik
+        document.getElementById("bigAssTaht").innerHTML=koik.slice(-9,-1)
     }
 });
