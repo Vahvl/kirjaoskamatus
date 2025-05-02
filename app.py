ad7f9e509540313c1af2029827008129b2de9de0
Vahvl <74665243+Vahvl@users.noreply.github.com>
2025-05-02 10:29:10 +0000

    ma lege ei tea mida ma tegin see tund ong noh vahelehed ja navbar ja veel midagi vist ma unustasin
    Co-authored-by: LennartLuud <LennartLuud@users.noreply.github.com>

--- app.py
+++ app.py
@@ -7,6 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/t1")
+def t1():
+    return render_template("tase1.html")
+
+@app.route("/t2")
+def t2():
+    return render_template("tase2.html")
+
+@app.route("/t3")
+def t3():
+    return render_template("tase3.html")
+
+@app.route("/meist")
+def meist():
+    return render_template("storage.html")
+  
+
 if __name__ == '__main__':
     app.run(debug=True)
 
--- static/main.js
+++ static/main.js
@@ -6,11 +6,12 @@ document.addEventListener('keydown', (event) => {
     if (keyPressed == "Backspace") {
         koik = koik.slice(0, -1)
         document.getElementById("koikTahed").innerHTML=koik
-        document.getElementById("bigAssTaht").innerHTML=koik.slice(-9,-1)
+        document.getElementById("bigAssTaht").innerHTML=koik.slice(-11)
     }
     else if (keyPressed.length==1) {
         koik+=keyPressed
         document.getElementById("koikTahed").innerHTML=koik
-        document.getElementById("bigAssTaht").innerHTML=koik.slice(-9,-1)
+        document.getElementById("bigAssTaht").innerHTML=koik.slice(-11,-1)+keyPressed
     }
 });
+
